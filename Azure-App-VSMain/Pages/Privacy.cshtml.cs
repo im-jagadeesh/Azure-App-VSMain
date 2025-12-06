0038a096a9dcebc558bc969b7dac893ec6b3b4b1
Jagadeesh Sivapuram <jagadeesh@SIVAPURAM-LAPPY>
2025-12-06 23:32:02 +0530

    Azur config changes testing

--- Azure-App-VSMain/Pages/Privacy.cshtml.cs
+++ Azure-App-VSMain/Pages/Privacy.cshtml.cs
@@ -6,14 +6,20 @@ namespace Azure_App_VSMain.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger, IConfiguration configuration )
         {
-            _logger = logger;
+            _logger = logger;            
+            _configuration = configuration;
+
         }
 
         public void OnGet()
         {
+            //ViewData["Greethings"] = _configuration.GetValue<string>("Greethings");
+            ViewData["Greethings"] = _configuration["Greethings"];
+            //throw new Exception("This is a test exception for demonstration purposes for Azure");
         }
     }
 
